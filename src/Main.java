f62e232657b3983e3e80e5568af2a83f7cf5d143
seonjae <tjswo2292@naver.com>
2024-10-03 13:25:57 +0900

    프로그래머스 k번째 수

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,31 @@
 import java.io.*;
 import java.util.Arrays;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
 
-        int a = Integer.parseInt(br.readLine());
+        Stack<Integer> result = new Stack<>();
 
-        String[] input = br.readLine().split(" ");
-        int[] nums = new int[a];
+        for (int i = 0; i < commands.length; i++) {
+            int[] test1 = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
 
-        for (int j = 0; j < nums.length; j++) {
-            nums[j] = Integer.parseInt(input[j]);
+            Arrays.sort(test1);
+
+            result.push(test1[commands[i][2] - 1]);
         }
-        Arrays.sort(nums);
-
-        int sum = 0;
-        int result = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            result += sum;
-        }
-
-        System.out.println(result);
     }
 }
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        int a = Integer.parseInt(br.readLine());
+//
+//        String[] input = br.readLine().split(" ");
+//        int[] nums = new int[a];
+//
+//        for (int j = 0; j < nums.length; j++) {
+//            nums[j] = Integer.parseInt(input[j]);
+//        }
+
