bb860a8b0b430a16db4a1e628c64da213ab7239d
seonjae <tjswo2292@naver.com>
2024-10-03 11:03:43 +0900

    백준 11399 ATM 선택정렬

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,28 @@
 import java.io.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-
         int a = Integer.parseInt(br.readLine());
 
         String[] input = br.readLine().split(" ");
-        int[] nums = new int[input.length];
+        int[] nums = new int[a];
 
         for (int j = 0; j < nums.length; j++) {
             nums[j] = Integer.parseInt(input[j]);
         }
+        Arrays.sort(nums);
 
-        int count = 0;
+        int sum = 0;
+        int result = 0;
 
-        for (int i = 0; i < input.length; i++) {
-            for (int j = 0; j < input.length - 1; j++) {
-                if(nums[j] > nums[j+1]) {
-                    int temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
-                    count++;
-                }
-            }
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            result += sum;
         }
 
-        System.out.println(count);
+        System.out.println(result);
     }
 }
